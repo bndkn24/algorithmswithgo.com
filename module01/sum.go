6c213ad71307b306e4b2777d350608f393bb71ec
bndkn24 <deekshithnetha24@gmail.com>
2022-01-07 21:55:47 +0530

    Update go.mod and NumInList and Sum exercises

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,10 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
+	// var sum int
+	// for _, i := range numbers {
+	// 	sum += i
+	// }
+	// return sum
 	return 0
 }
